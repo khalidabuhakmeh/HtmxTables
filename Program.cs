a2121bc133cc8ec10934b1dbcd0b47d28731fe2b
Khalid Abuhakmeh <khalidabuhakmeh@gmail.com>
2021-08-06 14:13:54 -0400

    Add Runtime Razor Compilation

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Hosting;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services
     .AddRazorPages()
+    .AddRazorRuntimeCompilation()
     // for htmx-form validation
     .AddViewOptions(options =>
     {
